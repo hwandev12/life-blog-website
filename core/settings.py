6760d9f2489d5131197b80ab0ba21352423708a9
Husan <102412893+hwandev12@users.noreply.github.com>
2022-05-13 08:54:40 +0500

    initial styles

--- core/settings.py
+++ core/settings.py
@@ -106,6 +106,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+STATIC_ROOT = 'staticfiles'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
